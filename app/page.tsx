b773117e9a0c0db9686e35693f2b2f020fdff125
Jacob Boyce <jboyce313@gmail.com>
2025-05-16 09:35:37 -0500

    added section header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { parisienne } from '@/app/fonts';
+import SectionHeader from './ui/section-header';
 
 export default function Home() {
   return (
     <main>
       <h1 className={`${parisienne.className}`}>CineFiles</h1>
       <p>Movie reviews and essays</p>
+      <SectionHeader title="Latest Reviews" />
     </main>
   );
 }
---
+++ app/ui/section-header.tsx
@@ -0,0 +1,7 @@
+export default function SectionHeader({title}: {title: string}) {
+    return (
+        <>
+        <h2>{title}</h2>
+        </>
+    );
+}
